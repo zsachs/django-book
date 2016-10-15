11e585c0a363cb5cfc6452309e2f1768c62d3941
zsachs <zsachs@users.noreply.github.com>
2016-10-15 10:28:01 -0500

    Point URLconf to first view

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from mysite.views import hello
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello),
 ]
