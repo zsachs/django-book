ae733af0dece03932c02e008e5cd10dc9fd8b119
zsachs <zsachs@users.noreply.github.com>
2016-10-25 20:43:28 -0500

    Define, install, and migrate book models

---
+++ books/migrations/0001_initial.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('first_name', models.CharField(max_length=30)),
+                ('last_name', models.CharField(max_length=40)),
+                ('email', models.EmailField(max_length=254)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('title', models.CharField(max_length=100)),
+                ('publication_date', models.DateField()),
+                ('authors', models.ManyToManyField(to='books.Author')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Publisher',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('name', models.CharField(max_length=30)),
+                ('address', models.CharField(max_length=50)),
+                ('city', models.CharField(max_length=60)),
+                ('state_province', models.CharField(max_length=30)),
+                ('country', models.CharField(max_length=50)),
+                ('website', models.URLField()),
+            ],
+        ),
+        migrations.AddField(
+            model_name='book',
+            name='publisher',
+            field=models.ForeignKey(to='books.Publisher'),
+        ),
+    ]
