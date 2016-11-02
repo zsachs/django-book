391bc441c9deae844f8a0f13f92eaa2022b7c9d8
zsachs <zsachs@users.noreply.github.com>
2016-11-01 21:14:31 -0500

    Make Book publication_date optional

---
+++ books/migrations/0002_auto_20161102_0209.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('books', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='publisher',
+            options={'ordering': ['name']},
+        ),
+        migrations.AlterField(
+            model_name='author',
+            name='email',
+            field=models.EmailField(max_length=254, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='book',
+            name='publication_date',
+            field=models.DateField(blank=True, null=True),
+        ),
+    ]
--- books/models.py
+++ books/models.py
@@ -29,7 +29,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
+    publication_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.title
