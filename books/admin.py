dd127308a46fd5730f350d61a1815d63c10fe1ab
zsachs <zsachs@users.noreply.github.com>
2016-11-01 21:31:45 -0500

    Customize Author change list

--- books/admin.py
+++ books/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Publisher, Author, Book
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
+
 admin.site.register(Publisher)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
 
