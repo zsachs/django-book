d04e79a3345b6c25de121db8e254701ce5d51725
zsachs <zsachs@users.noreply.github.com>
2016-11-09 06:23:51 -0600

    Make a dummy book search form

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from mysite.views import hello, current_datetime, hours_ahead#, display_meta
+from books import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -22,4 +23,6 @@ urlpatterns = [
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 #    url(r'^meta/$', display_meta),
+    url(r'^search-form/$', views.search_form),
+    url(r'^search/$', views.search),
 ]
