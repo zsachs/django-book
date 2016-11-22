d948cb18d902debc3fec345e5c236f280d865ce7
zsachs <zsachs@users.noreply.github.com>
2016-11-21 20:55:25 -0600

    Query Book database with form parameters

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from books.models import Book
 
 # Create your views here.
 
@@ -7,9 +8,11 @@ def search_form(request):
     return render(request, 'search_form.html')
 
 def search(request):
-    if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        books = Book.objects.filter(title__icontains=q)
+        return render(request, 'search_results.html',
+                        {'books': books, 'query': q})
     else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
+        return HttpResponse('Please submit a search term.')
 
