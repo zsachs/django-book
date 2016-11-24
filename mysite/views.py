455fc46eab4f30964f17e5a9f1a154cfc8d151c5
zsachs <zsachs@users.noreply.github.com>
2016-11-23 21:43:22 -0600

    Customize contact form validations and design

--- mysite/views.py
+++ mysite/views.py
@@ -42,6 +42,8 @@ def contact(request):
             )
             return HttpResponseRedirect('/contact/thanks/')
     else:
-        form = ContactForm()
+        form = ContactForm(
+            initial={'subject': 'I love your site!'}
+        )
     return render(request, 'contact_form.html', {'form': form})
 
